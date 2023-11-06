3557ea7ad7a6d1801d704bc97e9011af4415d49b
franAndrad <franciscoezequielandrade@gmail.com>
2023-11-06 16:30:41 -0300

    se agrego una prueba para tomar el tiempo

--- codigo10KB/tiempo.bat
+++ codigo10KB/tiempo.bat
@@ -1,18 +1,14 @@
 @echo off
 
-REM Configura la fecha y hora en las variables
-for /F "tokens=2" %%I in ('time /t') do set start_time=%%I
-for /F "tokens=1" %%I in ('date /t') do set start_date=%%I
-
-REM Inicio del programa
-echo Programa iniciado a: %start_time% %start_date%
+REM Captura la fecha y hora inicial
+echo Programa iniciado a: > timestamp.txt
+echo %time% >> timestamp.txt
+echo %date% >> timestamp.txt
 
 REM Ejecuta el programa
 c10k.exe
 
-REM Captura la hora y la fecha de finalización
-for /F "tokens=2" %%I in ('time /t') do set end_time=%%I
-for /F "tokens=1" %%I in ('date /t') do set end_date=%%I
-
-REM Muestra la hora y fecha de finalización
-echo Programa finalizado a: %end_time% %end_date%
+REM Captura la fecha y hora al finalizar el programa
+echo Programa finalizado a: >> timestamp.txt
+echo %time% >> timestamp.txt
+echo %date% >> timestamp.txt
